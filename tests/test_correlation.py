90c3368708e28f39becde0a4ec2674c0ae8262a2
SIKAI ZHANG <34108862+MatthewSZhang@users.noreply.github.com>
2024-07-09 13:32:09 +0800

    MNT add tests

--- tests/test_correlation.py
+++ tests/test_correlation.py
@@ -2,13 +2,15 @@
 """Test FastCan"""
 
 import numpy as np
-from sklearn.datasets import make_classification
-from sklearn.linear_model import LinearRegression
-from sklearn.utils._testing import (
+from numpy.testing import (
     assert_almost_equal,
     assert_array_equal,
+    assert_array_almost_equal,
 )
-
+from sklearn.cross_decomposition import CCA
+from sklearn.datasets import make_classification, make_regression
+from sklearn.linear_model import LinearRegression
+import pytest
 from fastcan import FastCan
 
 
@@ -39,13 +41,174 @@ def test_select_kbest_classif():
     gtruth_ssc = reg.score(X[:, :n_informative], y)
 
     correlation_filter = FastCan(
-        n_features_to_select=5,
+        n_features_to_select=n_informative,
+    )
+    correlation_filter.fit(X, y)
+    ssc = correlation_filter.scores_.sum()
+    # Test whether the ssc from the fastcan is consistent
+    # with the mcc from the linear regression
+    assert_almost_equal(actual=ssc, desired=gtruth_ssc)
+
+    support = correlation_filter.get_support()
+    gtruth = np.zeros(n_features)
+    gtruth[:n_informative] = 1
+    assert_array_equal(support, gtruth)
+
+def test_inclusive_indices():
+    # Test whether fastcan can select informative features based
+    # on some pre-include features
+    n_samples = 20
+    n_features = 20
+    n_targets = 8
+    n_informative = 5
+
+    X, y = make_regression(
+        n_samples=n_samples,
+        n_features=n_features,
+        n_informative=n_informative,
+        n_targets=n_targets,
+        noise=0.1,
+        shuffle=False,
+        random_state=0,
+    )
+
+    correlation_filter = FastCan(
+        n_features_to_select=n_informative,
+        inclusive_indices=[0, 3]
+    )
+    correlation_filter.fit(X, y)
+
+    support = correlation_filter.get_support()
+    gtruth = np.zeros(n_features)
+    gtruth[:n_informative] = 1
+    assert_array_equal(support, gtruth)
+
+def test_ssc_consistent_with_cca():
+    # Test whether the ssc got from the fastcan is consistent
+    # with the ssc got from CCA
+    n_samples = 200
+    n_features = 20
+    n_targets = 10
+    n_informative = 10
+
+    X, y = make_regression(
+        n_samples=n_samples,
+        n_features=n_features,
+        n_informative=n_informative,
+        n_targets=n_targets,
+        noise=0.1,
+        shuffle=False,
+        random_state=0,
+    )
+
+    cca = CCA(n_components=n_targets)
+    cca.fit(X, y)
+    X_c, Y_c = cca.transform(X, y)
+    corrcoef = np.corrcoef(X_c, Y_c, rowvar=False).diagonal(offset=n_targets)
+    gtruth_ssc = sum(corrcoef**2)
+
+    correlation_filter = FastCan(
+        n_features_to_select=n_features,
     )
     correlation_filter.fit(X, y)
     ssc = correlation_filter.scores_.sum()
     assert_almost_equal(actual=ssc, desired=gtruth_ssc)
 
-    support = correlation_filter.get_support()
-    gtruth = np.zeros(20)
-    gtruth[:5] = 1
-    assert_array_equal(support, gtruth)
+def test_h_eta_consistency():
+    # Test whether the ssc got from h-correlation is
+    # consistent with the ssc got from eta-cosine
+    n_samples = 200
+    n_features = 20
+    n_targets = 10
+    n_informative = 10
+    n_to_select = 5
+
+    X, y = make_regression(
+        n_samples=n_samples,
+        n_features=n_features,
+        n_informative=n_informative,
+        n_targets=n_targets,
+        noise=0.1,
+        shuffle=False,
+        random_state=0,
+    )
+
+    h_correlation = FastCan(
+        n_features_to_select=n_to_select,
+        eta=False
+    )
+    eta_cosine = FastCan(
+        n_features_to_select=n_to_select,
+        eta=True
+    )
+    h_correlation.fit(X, y)
+    eta_cosine.fit(X, y)
+    assert_array_almost_equal(h_correlation.scores_, eta_cosine.scores_)
+
+def test_raise_errors():
+    # Test whether fastcan raise errors properly
+    n_samples = 20
+    n_features = 20
+    n_classes = 10
+    n_informative = 15
+    n_redundant = n_features-n_informative
+
+    X, y = make_classification(
+        n_samples=n_samples,
+        n_features=n_features,
+        n_informative=n_informative,
+        n_classes=n_classes,
+        n_redundant=n_redundant,
+        flip_y=0,
+        shuffle=False,
+        random_state=0,
+    )
+
+    selector_n_select = FastCan(
+        n_features_to_select=n_features+1,
+    )
+    selector_n_inclusive = FastCan(
+        n_features_to_select=n_features,
+        inclusive_indices=range(n_features+1)
+    )
+    selector_eta_for_small_size_samples = FastCan(
+        n_features_to_select=n_features,
+        eta=True
+    )
+
+    with pytest.raises(ValueError, match=r"n_features_to_select .*"):
+        selector_n_select.fit(X, y)
+
+    with pytest.raises(ValueError, match=r"n_inclusions .*"):
+        selector_n_inclusive.fit(X, y)
+
+    with pytest.raises(ValueError, match=r"`eta` cannot be True, .*"):
+        selector_eta_for_small_size_samples.fit(X, y)
+
+
+@pytest.mark.filterwarnings("ignore::pytest.PytestUnraisableExceptionWarning")
+def test_cython_errors():
+    # Test whether fastcan raise cython errors properly
+    rng = np.random.default_rng(0)
+    n_samples = 20
+    n_informative = 15
+    x_1 = rng.random((n_samples, n_informative))
+    x_2 = x_1[:, 0]
+    X = np.c_[x_1, x_2]
+    y = rng.random((n_samples))
+
+
+    selector_non_singular = FastCan(
+        n_features_to_select=n_informative+1,
+    )
+
+    with pytest.raises(RuntimeError, match="The selection is interrupted by error!!!"):
+        # No candidate
+        selector_non_singular.fit(X, y)
+
+    with pytest.raises(RuntimeError, match="The selection is interrupted by error!!!"):
+        # No improvement
+        selector_no_improve = FastCan(
+            n_features_to_select=2,
+        )
+        selector_no_improve.fit(np.zeros((3, 2)), np.zeros(3))
